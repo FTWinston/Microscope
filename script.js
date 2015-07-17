ba26b13167ad3815d6cadb2df385914f810eec01
FTWinston <ftwinston@gmail.com>
2015-07-17 14:48:22 +0100

    Added toolbox div, working on hover effects to indicate where insertions would occur

--- script.js
+++ script.js
@@ -2,4 +2,16 @@ $(function() {
 	$('#cardLayout').on('dblclick', '.card', function() {
 		$(this).toggleClass('light dark');
 	});
+	
+	$('#toolbox ul').on('click', 'li', function() {
+		var val = $(this).children('.value').text();
+		
+		var root = $('#cardLayout');
+		if (root.attr('mode') == val)
+			root.removeAttr('mode');
+		else {
+			root.attr('mode', val);
+			console.log('Entered \'add ' + val + '\' mode');
+		}
+	});
 });
