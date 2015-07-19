67530aa313f4700eaf7d5fca9e33a15bb5b0603e
FTWinston <ftwinston@gmail.com>
2015-07-20 00:42:29 +0100

    Can also add events and scenes by dragging
    Some issues remain with adding scenes: they start in the wrong position, and an error occurs if a new event has been added within a new scene (but only that combination)

--- script.js
+++ script.js
@@ -1,28 +1,73 @@
 $(function() {
-	$('#cardLayout').on('dblclick', '.card', function() {
+	$('#cardLayout').on('dblclick', '.card', function () {
 		$(this).toggleClass('light dark');
-	}).sortable({
-		handle: '.card.period',
-		items: '> .group.period',
-		placeholder: 'ui-state-highlight card placeholder',
-		connectWith: '#templates',
-		disabled: true,
-		stop: function () {
-			$('#cardLayout .group.period').removeAttr('style');
-			$('#cardLayout').sortable('option', 'disabled', true);
-		}
-    });
+	})
 	
-	$('#addPeriod').mousedown(function (e) {
-		var newCardGroup = $('<div/>', {class:'period group'})/*.draggable()*/.appendTo('#cardLayout');
-		var newCard = $('<div/>', {class:'period card light', contenteditable:'true'}).appendTo(newCardGroup);
+	// adding periods
+	setupAdding('#addPeriod', '#cardLayout', 'period', true, 48, 0);
+	setupSortable('#cardLayout', 'period', true, function (obj) {
+		// initialize event sorting within this new period
+		setupSortable('#cardLayout .group.period', 'event', true, undefined, obj);
+	});
+	
+	// adding events
+	setupAdding('#addEvent', '#cardLayout .group.period', 'event', true, 248, 0);
+	setupSortable('#cardLayout .group.period', 'event', true, function (obj) {
+		// initialize scene sorting within this new event
+		setupSortable('#cardLayout .group.event', 'scene', true, undefined, obj);
+	});
+	
+	// adding scenes
+	setupAdding('#addScene', '#cardLayout .group.event', 'scene', false, 258, 306);
+	setupSortable('#cardLayout .group.event', 'scene', false);
+});
+
+function setupSortable(rootSelector, cardType, useGroups, addComplete, rootObject) {
+	if (rootObject === undefined)
+		rootObject = $(rootSelector);
+
+	rootObject.sortable({
+			handle: useGroups ? '.card.' + cardType : undefined,
+			items: useGroups ? '> .group.' + cardType : '> .card.' + cardType,
+			placeholder: 'ui-state-highlight card ' + cardType + 'Placeholder',
+			connectWith: rootSelector,
+			disabled: true,
+			stop: function (e, ui) {
+				$('#cardLayout .' + (useGroups ? 'group' : 'card') + '.' + cardType).removeAttr('style');
+				$(rootSelector).sortable('option', 'disabled', true);
+				
+				if (addComplete !== undefined)
+					addComplete(ui.item);
+					
+				$('#newest').removeAttr('id');
+			}
+		});
+}
+
+function setupAdding(buttonSelector, rootSelector, cardType, useGroups, addOffsetX, addOffsetY) {
+	$(buttonSelector).mousedown(function (e) {
+		var appendTo = $(rootSelector + ':first');
+		var newCard = $('<div/>', {class: cardType + ' card light', contenteditable:'true'});
 		
-		$('#cardLayout').sortable('refresh').sortable('option', 'disabled', false);
+		var newRoot;
+		if (useGroups) {
+			var newCardGroup = $('<div/>', {class: cardType + ' group'}).appendTo(appendTo);
+			newCard.appendTo(newCardGroup);
+			newRoot = newCardGroup;
+		}
+		else {
+			newCard.appendTo(appendTo);
+			newRoot = newCard;
+		}
+		
+		appendTo.sortable('refresh');
+		$(rootSelector).sortable('option', 'disabled', false);
 		
 		var pos = $('#toolbox').position();
-		newCardGroup.css('position', 'absolute');
-		newCardGroup.css('left', pos.left);
-		newCardGroup.css('top', pos.top);
+		newRoot.attr('id', 'newest');
+		newRoot.css('position', 'absolute');
+		newRoot.css('left', pos.left + $(window).scrollLeft() - addOffsetX);
+		newRoot.css('top', pos.top + $(window).scrollTop() - addOffsetY);
 		
 		e.type = 'mousedown.sortable';
 		e.target = newCard[0];
@@ -30,4 +75,4 @@ $(function() {
 		
 		return false;
 	});
-});
+}
