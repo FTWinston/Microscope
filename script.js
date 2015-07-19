2a63ad037bd08955596edaa505d15d66f94069af
FTWinston <ftwinston@gmail.com>
2015-07-19 23:04:07 +0100

    Can now place periods by dragging

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 $(function() {
 	$('#cardLayout').on('dblclick', '.card', function() {
 		$(this).toggleClass('light dark');
-	});
-	
-	$('#toolbox ul').on('click', 'li', function() {
-		var val = $(this).children('.value').text();
-		
-		var root = $('#cardLayout');
-		if (root.attr('mode') == val)
-			root.removeAttr('mode');
-		else {
-			root.attr('mode', val);
-			console.log('Entered \'add ' + val + '\' mode');
+	}).sortable({
+		handle: '.card.period',
+		items: '> .group.period',
+		placeholder: 'ui-state-highlight card placeholder',
+		connectWith: '#templates',
+		disabled: true,
+		stop: function () {
+			$('#cardLayout .group.period').removeAttr('style');
+			$('#cardLayout').sortable('option', 'disabled', true);
 		}
+    });
+	
+	$('#addPeriod').mousedown(function (e) {
+		var newCardGroup = $('<div/>', {class:'period group'})/*.draggable()*/.appendTo('#cardLayout');
+		var newCard = $('<div/>', {class:'period card light', contenteditable:'true'}).appendTo(newCardGroup);
+		
+		$('#cardLayout').sortable('refresh').sortable('option', 'disabled', false);
+		
+		var pos = $('#toolbox').position();
+		newCardGroup.css('position', 'absolute');
+		newCardGroup.css('left', pos.left);
+		newCardGroup.css('top', pos.top);
+		
+		e.type = 'mousedown.sortable';
+		e.target = newCard[0];
+		newCard.trigger(e);
+		
+		return false;
 	});
 });
